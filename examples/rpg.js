f55927fab0b7b2d81e969b5d9fb04076ace16d00
mateuszjansowa <mateusz.jan.sowa@gmail.com>
2023-03-05 11:29:55 +0100

    rpg example

---
+++ examples/rpg.js
@@ -0,0 +1,38 @@
+// CharacterFactory to Funkcja Wytwórcza
+import { characterFactory } from './characterFactory';
+
+const humanPlayer = characterFactory({
+    type: 'human',
+    level: 0,
+    money: 0,
+    health: 100
+});
+
+const shopNPC = characterFactory({
+    type: 'shop',
+    money: 100,
+    health: null
+});
+
+// dzięki wspólnym cechom obiektów możemy je grupować
+const players = [humanPlayer, shopNPC];
+
+// możemy użyć współdzielone metody
+humanPlayer.renderOnMap({
+    x: 0,
+    y: 0
+});
+
+// możemy użyć współdzielone metody
+shopNPC.renderOnMap({
+    x: 10,
+    y: 10
+});
+
+// możemy użyć współdzielone metody
+for (const player in players) {
+    player.renderOnMap({
+        x: Math.random() * 10,
+        y: Math.random() * 10
+    });
+}
